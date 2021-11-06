826c8544e3f592aa6d136d9ea4edb8e13a10affd
Michael Rider <mikepdxrider@gmail.com>
2021-11-05 23:07:20 -0700

    implements AWS SDK SES

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,24 +1,41 @@
+// import ModelClass
+
+// import the sendEmail function
+const sendEmail = require('../lib/utils/aws-ses/send-email.js');
+// reassign the function to jest.fn();
+
 const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
-const request = require('supertest');
-const app = require('../lib/app.js');
+// const request = require('supertest');
+// const app = require('../lib/app.js');
 
 describe('07-middleware-service-layer routes', () => {
   beforeEach(() => {
+    // clear the mock call value before each test
+
     return setup(pool);
   });
 
-  // test post matches getById route 
-  it('matches a post request body to a getById response body', async () => {
-    // declare a new object variable.
+  // // test post matches getById route 
+  // it('matches a post request body to a getById response body & sends an email', async () => {
+  //   // declare a new object variable.
 
-    // await a post request to </route>
-    //  - attach a send(newObjVar)
+  //   // await a post request to </route>
+  //   //  - attach a send(newObjVar)
 
-    // declare a new variable to save an async get request to </route/:id>
+  //   // declare a new variable to save an async get request to </route/:id>
 
-    // expect getbyIdResponse to equal the new object with an id: expect.any(String)
-  });
+  //   // expect getbyIdResponse to equal the new object with an id: expect.any(String)
+  //   // expect sendEmail to have been called once.
+  // });
+
+
+  it('tests ses send attempt', async () => {
+    const sesResponse = await sendEmail('Hello', 'mikepdxrider@gmail.com');
+      
+    expect(sesResponse).toEqual({});
+  }, 10000);
+
 
   // test getAll route
 
@@ -26,6 +43,7 @@ describe('07-middleware-service-layer routes', () => {
 
   // test delete route
 
+  
   afterAll(() => {
     pool.end();
   });
